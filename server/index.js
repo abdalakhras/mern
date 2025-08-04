034b6ae6218911d5dafc3a91f581ed837bd97972
abdalakhras <abdakalakhras@gmail.com>
2025-08-05 00:32:01 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,30 @@ app.use(express.json());
 
 app.use('/api/users',userRouter)
 
+// app.get("/hello/:number1/:number2",(req,res)=>{
+//     const num1 = req.params.number1
+//     const num2 = req.params.number2
+//     const total =  Number(num1)+Number(num2)
+//      console.log(num1,"/",num2,'=',total)
+// res.send(`welcom to local host in hello page where number1 and number2 ${num1}/${num2} and the total is ${total}`)
+// })
+app.get("/hey",(req,res)=>{
+    res.send("this is a response from the local host server : you visited hey path")
+})
+app.put("/test",(req,res)=>{
+    res.send("this is a response from localhost : you are on test page")
+})
+app.get("/kjkj",(req,res)=>{
+    // res.send(`the server response for the get request is my name :  ${req.body.name}`)
+    res.json({
+        name : req.body.name,
+        age : req.body.age, 
+        Password:'not available'
+    })
+    console.log(req.body.age)
+})
+
+
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
 
