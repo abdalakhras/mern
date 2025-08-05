d516312ca4d7300fe00b931d4dd408a4a935f274
abdalakhras <abdakalakhras@gmail.com>
2025-08-06 01:01:12 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const connectDB = require('./config/db')
 const dotenv = require('dotenv')
 
+const User = require('./models/user')
+
 const userRouter  = require('./routers/userRouters')
 
 dotenv.config()
@@ -22,9 +24,40 @@ app.use('/api/users',userRouter)
 app.get("/hey",(req,res)=>{
     res.send("this is a response from the local host server : you visited hey path")
 })
-app.put("/test",(req,res)=>{
-    res.send("this is a response from localhost : you are on test page")
+app.put("/test", async (req,res)=>{
+
+    // const userName = req.body.name
+    const Email = req.body.email
+    const Password = req.body.password
+
+    const newUser = new User({
+        username :req.body.name,
+        email:Email,
+        password : Password,
+    })
+    await newUser.save()
+    res.send("data is being stored in mongoDB")
 })
+
+app.get("/test",async(req,res)=>{
+ const xUSer = await User.find()
+ res.json(xUSer)
+})
+
+app.delete("/test/:testId",async(req,res)=>{
+    const id = req.params.testId
+    try{
+ const xUSer = await User.findByIdAndDelete(id)
+ res.json(xUSer)
+ return
+    }catch(error){
+        console.log("error")
+        res.send(error)
+        return
+    }
+})
+
+
 app.get("/kjkj",(req,res)=>{
     // res.send(`the server response for the get request is my name :  ${req.body.name}`)
     res.json({
