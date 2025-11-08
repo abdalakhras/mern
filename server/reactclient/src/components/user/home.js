7b42f285d69eba24457a79cbe53711c598204211
abdalakhras <abdakalakhras@gmail.com>
2025-11-08 09:58:57 +0300

    reactclient setup

---
+++ server/reactclient/src/components/user/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = ()=>{
+    return(
+        <div>
+        <h1>welcome to user homepage</h1>
+        <p>this is user home page</p>
+        </div>
+    )
+}
+export default Home
